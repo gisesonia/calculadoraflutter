1205a44b72af6357193b0c1d67e51ba8fd962111
Gisele <gisesonia@gmail.com>
2021-01-21 19:37:34 -0300

    expandindo os botões com expanded

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -10,6 +10,7 @@ class Button extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
+      flex: 1,
       child: RaisedButton(
         child: Text(text),
         onPressed: () {},
