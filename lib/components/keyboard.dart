06426094899ca829d10d00ed24264a53ccd7e1db
Gisele <gisesonia@gmail.com>
2021-01-21 19:58:01 -0300

    Adicionando constantes de cores

--- lib/components/keyboard.dart
+++ lib/components/keyboard.dart
@@ -11,15 +11,36 @@ class Keyboard extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ButtonRow([
+            Button(
+              text: 'AC',
+              big: true,
+            ),
+            Button(text: '%'),
+            Button(text: '/'),
+          ]),
+          ButtonRow([
             Button(text: '7'),
             Button(text: '8'),
             Button(text: '9'),
+            Button(text: 'x')
           ]),
           ButtonRow([
             Button(text: '4'),
             Button(text: '5'),
             Button(text: '6'),
+            Button(text: '-')
           ]),
+          ButtonRow([
+            Button(text: '1'),
+            Button(text: '2'),
+            Button(text: '3'),
+            Button(text: '+')
+          ]),
+          ButtonRow([
+            Button(text: '0', big: true),
+            Button(text: '.'),
+            Button(text: '='),
+          ])
         ],
       ),
     );
