d9deada393f3af2a436996da2601605dfa23960e
Gisele <gisesonia@gmail.com>
2021-01-21 20:23:18 -0300

    Convertendo calculator em stateful

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -2,7 +2,12 @@ import 'package:calculator/components/keyboard.dart';
 import 'package:flutter/material.dart';
 import '../components/display.dart';
 
-class Calculator extends StatelessWidget {
+class Calculator extends StatefulWidget {
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
   _onPressed(String text) {
     print(text);
   }
