ad08223b9c91e13f236f6326d46f5abe2da93488
Gisele <gisesonia@gmail.com>
2021-01-23 17:07:45 -0300

    Ajustes nos cálculos

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -5,11 +5,16 @@ class Memory {
   int _bufferIndex = 0;
   String _operation;
   bool _wipeValue = false;
+  String _lastCommand;
 
   //Atributo privado
   String _value = '0';
 
   void applyCommand(String command) {
+    if (_isReplacingOperation(command)) {
+      _operation = command;
+      return;
+    }
     if (command == 'AC') {
       _allClear();
     } else if (operations.contains(command)) {
@@ -17,23 +22,34 @@ class Memory {
     } else {
       _addDigit(command);
     }
+    _lastCommand = command;
+  }
+
+  _isReplacingOperation(String command) {
+    return operations.contains(_lastCommand) &&
+        operations.contains(command) &&
+        _lastCommand != '=' &&
+        command != '=';
   }
 
   _setOperation(String newOperation) {
+    bool isEqualSign = newOperation == '=';
     if (_bufferIndex == 0) {
-      _operation = newOperation;
-      _bufferIndex = 1;
+      if (!isEqualSign) {
+        _operation = newOperation;
+        _bufferIndex = 1;
+        _wipeValue = true;
+      }
     } else {
       _buffer[0] = _calculate();
       _buffer[1] = 0.0;
       _value = _buffer[0].toString();
       _value = _value.endsWith('.0') ? _value.split('.')[0] : _value;
 
-      bool isEqualSign = newOperation == '=';
       _operation = isEqualSign ? null : newOperation;
       _bufferIndex = isEqualSign ? 0 : 1;
     }
-    _wipeValue = true;
+    _wipeValue = true; //!isEqualSign;
   }
 
   _addDigit(String digit) {
