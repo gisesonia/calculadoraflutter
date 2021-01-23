3058a53510cacf54895076419715020fbc3e27a1
Gisele <gisesonia@gmail.com>
2021-01-23 16:48:12 -0300

    calculo das operações

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -23,6 +23,15 @@ class Memory {
     if (_bufferIndex == 0) {
       _operation = newOperation;
       _bufferIndex = 1;
+    } else {
+      _buffer[0] = _calculate();
+      _buffer[1] = 0.0;
+      _value = _buffer[0].toString();
+      _value = _value.endsWith('.0') ? _value.split('.')[0] : _value;
+
+      bool isEqualSign = newOperation == '=';
+      _operation = isEqualSign ? null : newOperation;
+      _bufferIndex = isEqualSign ? 0 : 1;
     }
     _wipeValue = true;
   }
@@ -47,9 +56,30 @@ class Memory {
 
   _allClear() {
     _value = '0';
+    _buffer.setAll(0, [0.0, 0.0]);
+    _bufferIndex = 0;
+    _operation = null;
+    _wipeValue = false;
   }
 
   String get value {
     return _value;
+  }
+
+  _calculate() {
+    switch (_operation) {
+      case '%':
+        return _buffer[0] % _buffer[1];
+      case '/':
+        return _buffer[0] / _buffer[1];
+      case 'x':
+        return _buffer[0] * _buffer[1];
+      case '-':
+        return _buffer[0] - _buffer[1];
+      case '+':
+        return _buffer[0] + _buffer[1];
+      default:
+        return _buffer[0];
+    }
   }
 }
